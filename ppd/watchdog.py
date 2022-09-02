2783e41deead940c3f5b745dc03db0aa1dd7ec1f
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-09-02 16:05:24 -0400

    Just a little work at work

--- ppd/watchdog.py
+++ ppd/watchdog.py
@@ -1,4 +1,27 @@
-from config import Config
+from pill_pack_dashboard.config import Config
+from .models import
+from
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
+
+path = Config.PATH_TO_WATCH
+
+def __init__(self, directory=".", handler=FileSystemEventHandler()):
+    self.observer = Observer()
+    self.handler = handler
+    self.directory = directory
+
+def run(self):
+    self.observer.schedule(self.handler, self.directory, recursive=True)
+    self.observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except:
+        self.observer.stop()
+    self.observer.join()
+
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(event) # Your code here
+        print(f'The file path is: {event.src_path}')
