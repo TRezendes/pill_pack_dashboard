3bdafea476a4c25f7016ee551b9ac9008166a1db
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-09-05 13:42:11 +0000

    Separated watcher and Fontend

--- ppd_flask/routes.py
+++ ppd_flask/routes.py
@@ -1,30 +1,7 @@
 from ppd import app
 from flask import render_template, redirect, url_for, flash, request
 from ppd.models import fill_lists
-
-listOfLists = [
-    ['LA_1st.dat', 'LA 1', 'Lakeview'],
-    ['LA_2nd.dat', 'LA 2', 'Lakeview'],
-    ['LA_3rd.dat', 'LA 3', 'Lakeview'],
-    ['LA_4th.dat', 'LA 4', 'Lakeview'],
-    ['RidgeA.dat', 'Ridge A', 'Tel Hai Skilled'],
-    ['RidgeB.dat', 'Ridge B', 'Tel Hai Skilled'],
-    ['RidgeC.dat', 'Ridge C', 'Tel Hai Skilled'],
-    ['BrookA.dat', 'Brook A', 'Tel Hai Skilled'],
-    ['BrookB.dat', 'Brook B', 'Tel Hai Skilled'],
-    ['BrookC.dat', 'Brook C', 'Tel Hai Skilled'],
-    ['Memory_Support.dat', 'LV', 'Meadowview'],
-    ['MtnView_Upper.dat', 'MV Upper', 'Mountainview Upper'],
-    ['MtnView_Lower.dat', 'MV Lower', 'Mountainview Lower'],
-    ['GSV_Skilled_FC.dat', 'FC', 'GSV Skilled'],
-    ['GSV_Skilled_WG.dat', 'WG', 'GSV Skilled'],
-    ['GSV_Skilled_SW.dat', 'SW', 'GSV Skilled'],
-    ['GSV_Skilled_SF.dat', 'SF', 'GSV Skilled'],
-    ['FMT_Farmcrest.dat', 'FA', 'Farmcrest'],
-    ['FMT_WRPC.dat', 'PC', 'WRPC'],
-    ['FMT_NE_NF.dat', 'NE, NF', 'FMT Skilled'],
-    ['FMT_NG_NH.dat', 'NG, NH', 'FMT Skilled']
-]
+import csv
 
 @app.route('/')
 def goToDashboard:
@@ -37,11 +14,15 @@ def Dashboard:
 @app.route('/reset')
 def dbReset:
     if form.validate_on_submit():
-        for list in listOfLists:
+        with open('../facilities.csv', newline='') as f:
+            reader = csv.reader(f)
+            facilityList = list(reader)
+        
+        for facility in facilityList:
             fill_list = fill_lists(
-                list_export_name=list[0],
-                display_name=list[1],
-                facility=list[2]
+                list_export_name=facility[0],
+                display_name=facility[1],
+                facility=facility[2]
             )
             db.session.add(fill_list)
             db.session.commit()
--- ppd_watchdog/watchdog.py
+++ ppd_watchdog/watchdog.py
@@ -1,10 +1,24 @@
 from pill_pack_dashboard.config import Config
 from ppd_flask.models import fill_lists
-import sqlalchemy
+from sqlalchemy import create_engine
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import platform
 
+dbURL = Config.SQLALCHEMY_DATABASE_URI
+engine = create_engine(DBURL, echo=True, future=True)
+opSys = platform.system()
 path = Config.PATH_TO_WATCH
+
+if opSys == 'Darwin':
+    driver = webdriver.Safari()
+else:
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service as ChromeService
+    from webdriver_manager.chrome import ChromeDriverManager
+    
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
 
 def __init__(self, directory=".", handler=FileSystemEventHandler()):
     self.observer = Observer()
