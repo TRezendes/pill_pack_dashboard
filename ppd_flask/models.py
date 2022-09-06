c7d097dc904626fbe6daa226bb798cc7484638c6
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-09-06 16:26:44 -0400

    Removed display_name column unique constraint (for now).

--- ppd_flask/models.py
+++ ppd_flask/models.py
@@ -1,8 +1,8 @@
 from ppd_flask import db
 
 class fill_lists(db.Model):
-    list_export_name = db.Column(db.Text, primary _key=True)
-    display_name = db.Column(db.Text, nullable=False, unique=True)
+    list_export_name = db.Column(db.Text, primary_key=True)
+    display_name = db.Column(db.Text, nullable=False)
     facility = db.Column(db.Text)
     exported = db.Column(db.Boolean, default=False)
     running = db.Column(db.Boolean, default=False)
