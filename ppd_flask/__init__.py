3b552630b5a5c90bf96715836a657da1c1f82427
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-09-06 16:28:05 -0400

    Reset route should work as expected now.

--- ppd_flask/__init__.py
+++ ppd_flask/__init__.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 app.config['SECRET_KEY'] = Config.SECRET_KEY
-app = Flask(__name__)
 db = SQLAlchemy(app)
 
 
-from ppd import routes
+#from ppd_flask import routes
