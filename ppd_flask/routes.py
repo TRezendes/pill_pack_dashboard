ff6f801b5e7fe0f22a2475312f2d4a1e29fef554
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-09-07 02:12:22 +0000

    Gotta initialize that list if it's going to work…

--- ppd_flask/routes.py
+++ ppd_flask/routes.py
@@ -18,16 +18,13 @@ def dbReset:
     if form.validate_on_submit():
         fill_lists.query.delete()
         db.session.commit()
+        fac_obj_list=[]
         with open('../facilities.csv', newline='') as f:
             reader = csv.reader(f)
             facilityList = list(reader)
 
         for facility in facilityList:
-            fac_dict = {
-            'list_export_name': facility[0],
-            'display_name': facility[1],
-            'facility': facility[2]
-            }
+            fac_dict = {'list_export_name': facility[0],'display_name': facility[1],'facility': facility[2]}
             fac_obj = fill_lists(**fac_dict)
             fac_obj_list.append(fac_obj)
         db.session.add_all(fac_obj_list)
